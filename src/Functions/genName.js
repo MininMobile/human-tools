a28900e03e8fde6e49a8b2ad2a8141f364149075
MininMobile <miningamer@gmail.com>
2018-05-25 23:24:26 +0100

    Tabs > Spaces

--- src/Functions/genName.js
+++ src/Functions/genName.js
@@ -1,12 +1,12 @@
 const names = require("./../JSON/names.json")
 
 function genName() {
-    function rand(min, max) { return Math.floor(Math.random() * max) + min; }
-    
-    let first = names.firstNames[rand(0, names.firstNames.length)];
-    let last = names.lastNames[rand(0, names.lastNames.length)];
+	function rand(min, max) { return Math.floor(Math.random() * max) + min; }
+	
+	let first = names.firstNames[rand(0, names.firstNames.length)];
+	let last = names.lastNames[rand(0, names.lastNames.length)];
 
-    return {"first":first, "last":last};
+	return {"first":first, "last":last};
 }
 
 module.exports = genName;

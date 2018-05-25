80fd75b8ad823ba2646986decefb862e391e16aa
MininMobile <miningamer@gmail.com>
2018-05-26 00:23:55 +0100

    Add Utils to exports

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = {
+	"Utils":{
+		"transformTime":require("./src/Util/trans"),
+		"generateName":require("./src/Util/genName"),
+		"random":require("./src/Util/rand")
+	},
+
 	"Person":require("./src/Classes/Person")
 }
